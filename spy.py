25e4fd347baada04bb5be35736bcd5c15d58a0fe
eso <27629096+2sn0w@users.noreply.github.com>
2020-11-21 23:15:18 -0500

    Update spy.py

--- spy.py
+++ spy.py
@@ -1,11 +1,12 @@
+#coding=UTF-8
 import discord
 import requests
 import asyncio
 
 client = discord.Client()
 
-spyChannel = CHANNELID
-webhookUrl = "WEBHOOKURL"
+spyChannels = [1234, 4321, 1337]
+webhookUrl = "https://discord.com/api/webhooks/FAKE/WEBHOOK"
 
 @client.event
 async def on_ready():
@@ -13,12 +14,12 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  if message.channel.id == spyChannel:
+  if message.channel.id in spyChannels:
     if message.attachments:
       attachment = message.attachments[0]
-      requests.post(webhookUrl, data={'content': message.content + "\n*Attachment: " + attachment.url + "*", 'username': message.author.name, 'avatar_url' : str(message.author.avatar_url)})
+      requests.post(webhookUrl, data={'content': message.content + "\n*Attachment: " + attachment.url + "*", 'username': message.author.name + ' [#' + message.channel.name + ']', 'avatar_url' : str(message.author.avatar_url)})
     else:
-      requests.post(webhookUrl, data={'content': message.content, 'username': message.author.name, 'avatar_url' : str(message.author.avatar_url)})
+      requests.post(webhookUrl, data={'content': message.content, 'username': message.author.name + ' [#' + message.channel.name + ']', 'avatar_url' : str(message.author.avatar_url)})
 
 print('Starting...')
-client.run("token", bot=False)
+client.run("TOKEN", bot=False)
